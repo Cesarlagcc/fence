54c65fe7affdb3e36076ac6b828cb842ad22d614
Cesarlagcc <cesarandon092@gmail.com>
2018-04-09 01:43:38 -0400

    updated width on services

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(window).scroll(function () {
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 697) {
+    if ($(window).scrollTop() > 862) {
       $('.flex-container').addClass('navbar-fixed');
       $('body').css("margin-top","50px")
     }
-    if ($(window).scrollTop() < 700) {
+    if ($(window).scrollTop() < 866) {
       $('.flex-container').removeClass('navbar-fixed');
       $('body').css("margin-top","0")
     }
