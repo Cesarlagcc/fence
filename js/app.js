0f44bd6d44cfd34b6ab667264e6a0b6f02138a61
Cesarlagcc <cesarandon092@gmail.com>
2018-04-09 01:32:12 -0400

    added header image

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(window).scroll(function () {
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 141) {
+    if ($(window).scrollTop() > 863) {
       $('.flex-container').addClass('navbar-fixed');
       $('body').css("margin-top","50px")
     }
-    if ($(window).scrollTop() < 144) {
+    if ($(window).scrollTop() < 866) {
       $('.flex-container').removeClass('navbar-fixed');
       $('body').css("margin-top","0")
     }
