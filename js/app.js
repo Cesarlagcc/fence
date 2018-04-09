cd3e85657ecf64524b75560e8f13df8e89ad857e
Cesarlagcc <cesarandon092@gmail.com>
2018-04-09 01:38:50 -0400

    modified scrolltop

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(window).scroll(function () {
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 693) {
+    if ($(window).scrollTop() > 697) {
       $('.flex-container').addClass('navbar-fixed');
       $('body').css("margin-top","50px")
     }
-    if ($(window).scrollTop() < 697) {
+    if ($(window).scrollTop() < 700) {
       $('.flex-container').removeClass('navbar-fixed');
       $('body').css("margin-top","0")
     }
