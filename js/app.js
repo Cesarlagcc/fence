f15e5c0bb53b9b8514ecc2315a9ee26089114d9c
Cesarlagcc <cesarandon092@gmail.com>
2018-04-09 01:34:14 -0400

    updated fixed nav

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(window).scroll(function () {
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 863) {
+    if ($(window).scrollTop() > 693) {
       $('.flex-container').addClass('navbar-fixed');
       $('body').css("margin-top","50px")
     }
-    if ($(window).scrollTop() < 866) {
+    if ($(window).scrollTop() < 697) {
       $('.flex-container').removeClass('navbar-fixed');
       $('body').css("margin-top","0")
     }
